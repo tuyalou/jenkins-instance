b840cee0ac300a24c77daab1dae13256c1f9edc3
EC2 Default User <ec2-user@ip-172-31-33-90.ec2.internal>
2020-07-16 19:18:28 +0000

    AMI image updated

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -1,9 +1,33 @@
 provider "aws" {}
 
+data "aws_ami" "centos" {
+  most_recent = true
+  owners      = ["679593333241"]
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+  }
+}
+
+
+
 resource "aws_instance" "jenkins" {
-  ami           = "ami-08f3d892de259504d"
+  ami           = "${data.aws_ami.centos.id}"
   instance_type = "t2.micro"
   tags = {
     Name = "Jenkins"
   }
 }
+
+  # resource "aws_instance" "jenkins" {
+  #   ami           = "ami-08f3d892de259504d"
+  #   instance_type = "t2.micro"
+  #   tags = {
+  #     Name = "Jenkins"
+  #   }
+  # }
+  
