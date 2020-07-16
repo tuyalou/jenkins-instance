1d21e7b4d6b65994e39b84d91825abb0520e5bae
EC2 Default User <ec2-user@ip-172-31-33-90.ec2.internal>
2020-07-16 20:30:10 +0000

    key_pair excluded

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -15,15 +15,15 @@ data "aws_ami" "centos" {
   }
 }
 
-resource "aws_key_pair" "jenkins_key" {
-  key_name   = "cluster"
-  public_key = "${file("/home/tuubayalcin/.ssh/id_rsa.pub")}"
-}
+# resource "aws_key_pair" "jenkins_key" {
+#   key_name   = "cluster"
+#   public_key = "${file("/home/tuubayalcin/.ssh/id_rsa.pub")}"
+# }
 
 resource "aws_instance" "jenkins" {
   ami           = "${data.aws_ami.centos.id}"
   instance_type = "t2.micro"
-  key_name      = "${aws_key_pair.jenkins_key.key_name}"
+  # key_name      = "${aws_key_pair.jenkins_key.key_name}"
   tags = {
     Name = "Jenkins"
   }
