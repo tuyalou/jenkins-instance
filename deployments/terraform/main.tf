637e07015e4e72d63c439283b114e67b2dbd8e43
EC2 Default User <ec2-user@ip-172-31-33-90.ec2.internal>
2020-07-16 19:44:53 +0000

    key_pair fixed

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -24,18 +24,11 @@ resource "aws_instance" "jenkins" {
   ami           = "${data.aws_ami.centos.id}"
   instance_type = "t2.micro"
   key_name      =   "${aws_key_pair.us-east-1-key.key_name}"
-  provisioner "remote-exec" {
-    connection {
-        type            = "ssh"
-        user            = "centos"
-        private_key     = "${file("~/.ssh/id_rsa")}"
-        host            = "${self.public_ip}"
-    }
   tags = {
     Name = "Jenkins"
   }
-  }
 }
+
 
   # resource "aws_instance" "jenkins" {
   #   ami           = "ami-08f3d892de259504d"
