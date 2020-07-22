8636e2f3de271015898810c49071cb8fdf52a8dd
tuyalou <63755131+tuyalou@users.noreply.github.com>
2020-07-22 17:09:43 -0500

    ami changed

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -2,18 +2,18 @@ provider "aws" {
   version = "2.59"
 }
 
-data "aws_ami" "centos" {
-  most_recent = true
-  owners      = ["679593333241"]
-  filter {
-    name   = "state"
-    values = ["available"]
-  }
-  filter {
-    name   = "name"
-    values = ["CentOS Linux 7 x86_64 HVM EBS *"]
-  }
-}
+# data "aws_ami" "centos" {
+#   most_recent = true
+#   owners      = ["679593333241"]
+#   filter {
+#     name   = "state"
+#     values = ["available"]
+#   }
+#   filter {
+#     name   = "name"
+#     values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+#   }
+# }
 
 # resource "aws_key_pair" "jenkins_key" {
 #   public_key = "${file("/home/tuubayalcin/.ssh/id_rsa.pub")}"
@@ -21,7 +21,7 @@ data "aws_ami" "centos" {
 # }
 
 resource "aws_instance" "jenkins" {
-  ami           = "${data.aws_ami.centos.id}"
+  ami           = "${image_type}"
   instance_type = "t2.micro"
   # key_name      = "${aws_key_pair.jenkins_key.key_name}"
   tags = {
