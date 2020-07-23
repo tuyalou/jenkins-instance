c6fe9f8b75571561912e7a5780ce6bba117b3232
tuyalou <63755131+tuyalou@users.noreply.github.com>
2020-07-22 23:24:05 -0500

    ami fixed

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -21,14 +21,14 @@ provider "aws" {
 # }
 
 resource "aws_instance" "jenkins" {
-  ami           = "${var.image_type}"
+  ami           = "${var.ami}"
   instance_type = "t2.micro"
   # key_name      = "${aws_key_pair.jenkins_key.key_name}"
   tags = {
     Name = "Jenkins"
   }
 }
-variable "image_type" = {}
+variable "ami" = {}
 
 
   # resource "aws_instance" "jenkins" {
