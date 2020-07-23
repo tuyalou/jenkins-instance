4a46ac21d4fddc8429ef04e7ea50d6c2237b6676
tuyalou <63755131+tuyalou@users.noreply.github.com>
2020-07-23 00:57:32 -0500

    just another try added

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -21,14 +21,14 @@ provider "aws" {
 # }
 
 resource "aws_instance" "jenkins" {
-  ami           = "${var.ami_id}"
+  ami           = "${var.ami}"
   instance_type = "t2.micro"
   # key_name      = "${aws_key_pair.jenkins_key.key_name}"
   tags = {
     Name = "Jenkins"
   }
 }
-variable "ami_id" {}
+variable "ami" {}
 
 
   # resource "aws_instance" "jenkins" {
