a9603467c8ebda45c79fd7ff04e74342ee92f204
tuyalou <63755131+tuyalou@users.noreply.github.com>
2020-07-24 14:56:59 -0500

    variable added

--- deployments/terraform/main.tf
+++ deployments/terraform/main.tf
@@ -21,11 +21,11 @@ resource "aws_instance" "jenkins" {
   instance_type = "t2.micro"
   # key_name      = "${aws_key_pair.jenkins_key.key_name}"
   tags = {
-    Name = "Jenkins"
+    Name = "var.instance_name"
   }
 }
 variable "ami" {}
-
+variable "instance_name" {}
 
   # resource "aws_instance" "jenkins" {
   #   ami           = "ami-08f3d892de259504d"
